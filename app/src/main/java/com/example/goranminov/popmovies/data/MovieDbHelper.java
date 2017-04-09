9f90bc1f18401156bcf4471b5272ceedff7308ac
Goran Minov <goran.s.minov@gmail.com>
2017-04-09 15:25:27 +0100

    Resolved conflicts in the database

--- app/src/main/java/com/example/goranminov/popmovies/data/MovieDbHelper.java
+++ app/src/main/java/com/example/goranminov/popmovies/data/MovieDbHelper.java
@@ -12,7 +12,7 @@ public class MovieDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "movie.db";
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     public MovieDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,7 +28,8 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                         MovieContract.MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
                         MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE + " REAL NOT NULL, " +
                         MovieContract.MovieEntry.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
-                        MovieContract.MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " + ");";
+                        MovieContract.MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " +
+                        "UNIQUE (" + MovieContract.MovieEntry.COLUMN_MOVIE_ID + ") ON CONFLICT REPLACE);";
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
 
     }
