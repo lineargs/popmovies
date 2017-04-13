a481e582a39724cea2dd0bedd5c9c4c01659844a
Goran Minov <goran.s.minov@gmail.com>
2017-04-13 20:42:15 +0100

    Refactoring the Loader so can add multiple

--- app/src/main/java/com/example/goranminov/popmovies/DetailActivity.java
+++ app/src/main/java/com/example/goranminov/popmovies/DetailActivity.java
@@ -1,7 +1,6 @@
 package com.example.goranminov.popmovies;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
@@ -9,20 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.goranminov.popmovies.databinding.ActivityDetailBinding;
 import com.example.goranminov.popmovies.utilities.MovieDatabaseJsonUtils;
 import com.example.goranminov.popmovies.utilities.NetworkUtils;
-import com.squareup.picasso.Picasso;
 
 import java.net.URL;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String[]>{
 
@@ -31,7 +22,7 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     private String movieIdFromIntent;
     private MovieDetailsAdapter movieDetailsAdapter;
     private RecyclerView mRecyclerView;
-    private static final int LOADER_ID = 101;
+    private static final int MOVIE_INFO_LOADER_ID = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,51 +50,56 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
             mRecyclerView.setAdapter(movieDetailsAdapter);
             LoaderManager.LoaderCallbacks<String[]> callbacks = DetailActivity.this;
             Bundle bundle = null;
-            getSupportLoaderManager().initLoader(LOADER_ID, bundle, callbacks);
+            getSupportLoaderManager().initLoader(MOVIE_INFO_LOADER_ID, bundle, callbacks);
         }
     }
 
     @Override
     public Loader<String[]> onCreateLoader(int id, final Bundle args) {
-        return new AsyncTaskLoader<String[]>(this) {
+        if (id == MOVIE_INFO_LOADER_ID) {
+            return new AsyncTaskLoader<String[]>(this) {
 
-            String[] mMovieData = null;
+                String[] mMovieData = null;
 
-            @Override
-            protected void onStartLoading() {
-                if (mMovieData != null) {
-                    deliverResult(mMovieData);
-                } else {
-                    forceLoad();
+                @Override
+                protected void onStartLoading() {
+                    if (mMovieData != null) {
+                        deliverResult(mMovieData);
+                    } else {
+                        forceLoad();
+                    }
                 }
-            }
 
-            @Override
-            public String[] loadInBackground() {
-                String movieId = movieIdFromIntent;
-                URL url = NetworkUtils.buildDetailUrl(movieId);
-                try {
-                    String movieString = NetworkUtils.getMainResponseFromHttpUrl(url);
-                    String[] movieData = MovieDatabaseJsonUtils.getMovieDetailFromJson(DetailActivity.this,
-                            movieString);
-                    return movieData;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
+                @Override
+                public String[] loadInBackground() {
+                    String movieId = movieIdFromIntent;
+                    URL url = NetworkUtils.buildDetailUrl(movieId);
+                    try {
+                        String movieString = NetworkUtils.getMainResponseFromHttpUrl(url);
+                        String[] movieData = MovieDatabaseJsonUtils.getMovieDetailFromJson(DetailActivity.this,
+                                movieString);
+                        return movieData;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
                 }
-            }
 
-            public void deliverResult(String[] data) {
-                mMovieData = data;
-                super.deliverResult(data);
-            }
-        };
+                public void deliverResult(String[] data) {
+                    mMovieData = data;
+                    super.deliverResult(data);
+                }
+            };
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void onLoadFinished(Loader<String[]> loader, String[] data) {
-        movieDetailsAdapter.setMovieData(data);
-        if (data == null) {
+        if (loader.getId() == MOVIE_INFO_LOADER_ID) {
+            movieDetailsAdapter.setMovieData(data);
+        }else if (data == null && loader.getId() == MOVIE_INFO_LOADER_ID) {
             Toast.makeText(DetailActivity.this, R.string.error_message_display, Toast.LENGTH_LONG).show();
             movieDetailsAdapter.setMovieData(data);
         }
